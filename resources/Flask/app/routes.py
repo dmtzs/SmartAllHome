163901deb249a6820cd52761d8ff0a641ed9eec4
Diego MS <diego.mtzs00@gmail.com>
2021-11-21 21:29:43 -0600

    Update routes.py
    Updates in the endpoints, added abou us endpoint skeleton

--- resources/Flask/app/routes.py
+++ resources/Flask/app/routes.py
@@ -1,7 +1,7 @@
 try:
-    import datetime as dt
     from app import app
     from flask import render_template
+    from datetime import datetime as dt
 except ImportError as eImp:
     print(f"The following import ERROR occurred in file {__file__}: {eImp}")
 
@@ -9,10 +9,17 @@ except ImportError as eImp:
 @app.context_processor
 def dateNow():
     return {
-        "now": dt.datetime.utcnow()
+        "now": dt.now()
     }
 
 # -------------Endpoints-------------
 @app.route("/")# Welcome HTML template
 def index():
     return render_template("welcome.html", pageTitle= "Home")
+
+# @Description: Endpoint that is used for show contact information with us.
+@app.route("/aboutus")
+def contacto():
+    global correoGDCode, nomApp, versionApp, descripcionApp
+
+    return render_template('aboutus.html', correo=correoGDCode, nombreApp=nomApp, versionDeApp=versionApp, decApp=descripcionApp)
